ebcaeaa77985de4aee7f90bc77805e4a85cf9588
pya-h <p4ya.py@gmail.com>
2023-12-30 23:15:59 +0330

    roadmap page completed

--- resources/views/roadmap.blade.php
+++ resources/views/roadmap.blade.php
@@ -11,102 +11,132 @@
             <div class="tt-col-lg-12">
 
 
-
-
-                <!-- Begin tt-Heading
-                    ======================
-                    * Use class "tt-heading-xsm", "tt-heading-sm", "tt-heading-lg", "tt-heading-xlg" or "tt-heading-xxlg" to set caption size (no class = default size).
-                    * Use class "tt-heading-stroke" to enable stroke style.
-                    * Use class "tt-heading-center" to align tt-Heading to center.
-                    * Use prepared helper class "max-width-*" to add custom width if needed. Example: "max-width-800". More info about helper classes can be found in the file "helper.css".
-                    -->
-                <div class="tt-heading tt-heading-stroke tt-heading-xsm anim-fadeinup">
-                    <h2 class="tt-heading-title">Custom Heading</h2>
-                    <!-- You can use <br> to break a text line if needed -->
-                    <h3 class="tt-heading-subtitle">Heading Subtitle</h3>
-                </div>
-                <!-- End tt-Heading -->
-
-                <br>
-
-                <!-- Begin tt-Heading
-                    ======================
-                    * Use class "tt-heading-xsm", "tt-heading-sm", "tt-heading-lg", "tt-heading-xlg" or "tt-heading-xxlg" to set caption size (no class = default size).
-                    * Use class "tt-heading-stroke" to enable stroke style.
-                    * Use class "tt-heading-center" to align tt-Heading to center.
-                    * Use prepared helper class "max-width-*" to add custom width if needed. Example: "max-width-800". More info about helper classes can be found in the file "helper.css".
-                    -->
-                <div class="tt-heading tt-heading-stroke tt-heading-sm anim-fadeinup">
-                    <h2 class="tt-heading-title">Custom Heading</h2>
-                    <!-- You can use <br> to break a text line if needed -->
-                    <h3 class="tt-heading-subtitle">Heading Subtitle</h3>
-                </div>
-                <!-- End tt-Heading -->
-
-                <br>
-
-                <!-- Begin tt-Heading
-                    ======================
-                    * Use class "tt-heading-xsm", "tt-heading-sm", "tt-heading-lg", "tt-heading-xlg" or "tt-heading-xxlg" to set caption size (no class = default size).
-                    * Use class "tt-heading-stroke" to enable stroke style.
-                    * Use class "tt-heading-center" to align tt-Heading to center.
-                    * Use prepared helper class "max-width-*" to add custom width if needed. Example: "max-width-800". More info about helper classes can be found in the file "helper.css".
-                    -->
-                <div class="tt-heading tt-heading-stroke anim-fadeinup">
-                    <h2 class="tt-heading-title">Custom Heading</h2>
-                    <!-- You can use <br> to break a text line if needed -->
-                    <h3 class="tt-heading-subtitle">Heading Subtitle</h3>
-                </div>
-                <!-- End tt-Heading -->
-
-                <br>
-
-                <!-- Begin tt-Heading
-                    ======================
-                    * Use class "tt-heading-xsm", "tt-heading-sm", "tt-heading-lg", "tt-heading-xlg" or "tt-heading-xxlg" to set caption size (no class = default size).
-                    * Use class "tt-heading-stroke" to enable stroke style.
-                    * Use class "tt-heading-center" to align tt-Heading to center.
-                    * Use prepared helper class "max-width-*" to add custom width if needed. Example: "max-width-800". More info about helper classes can be found in the file "helper.css".
-                    -->
-                <div class="tt-heading tt-heading-stroke tt-heading-lg anim-fadeinup">
-                    <h2 class="tt-heading-title">Custom Heading</h2>
-                    <!-- You can use <br> to break a text line if needed -->
-                    <h3 class="tt-heading-subtitle">Heading Subtitle</h3>
-                </div>
-                <!-- End tt-Heading -->
-
-                <br>
-
-                <!-- Begin tt-Heading
-                    ======================
-                    * Use class "tt-heading-xsm", "tt-heading-sm", "tt-heading-lg", "tt-heading-xlg" or "tt-heading-xxlg" to set caption size (no class = default size).
-                    * Use class "tt-heading-stroke" to enable stroke style.
-                    * Use class "tt-heading-center" to align tt-Heading to center.
-                    * Use prepared helper class "max-width-*" to add custom width if needed. Example: "max-width-800". More info about helper classes can be found in the file "helper.css".
-                    -->
                 <div class="tt-heading tt-heading-stroke tt-heading-xlg anim-fadeinup">
-                    <h2 class="tt-heading-title">Custom Heading</h2>
+                    <h2 class="tt-heading-title">2020</h2>
                     <!-- You can use <br> to break a text line if needed -->
-                    <h3 class="tt-heading-subtitle">Heading Subtitle</h3>
+                    <h3 class="tt-heading-subtitle">Forming the idea  💡 26<br>(What is twenty six? ...)</h3>
+                </div>
+                <!-- End tt-Heading -->
+
+                <br>
+                <div class="tt-heading tt-heading-stroke tt-heading-xlg anim-fadeinup">
+                    <h2 class="tt-heading-title">2021</h2>
+                    <!-- You can use <br> to break a text line if needed -->
+                    <h3 class="tt-heading-subtitle">Research about various projects and blockchains</h3>
                 </div>
                 <!-- End tt-Heading -->
 
                 <br>
 
-                <!-- Begin tt-Heading
-                    ======================
-                    * Use class "tt-heading-xsm", "tt-heading-sm", "tt-heading-lg", "tt-heading-xlg" or "tt-heading-xxlg" to set caption size (no class = default size).
-                    * Use class "tt-heading-stroke" to enable stroke style.
-                    * Use class "tt-heading-center" to align tt-Heading to center.
-                    * Use prepared helper class "max-width-*" to add custom width if needed. Example: "max-width-800". More info about helper classes can be found in the file "helper.css".
-                    -->
-                <div class="tt-heading tt-heading-stroke tt-heading-xxlg anim-fadeinup">
-                    <h2 class="tt-heading-title">Custom Heading</h2>
+                <div class="tt-heading tt-heading-stroke tt-heading-xlg anim-fadeinup">
+                    <h2 class="tt-heading-title">Feb 2022</h2>
                     <!-- You can use <br> to break a text line if needed -->
-                    <h3 class="tt-heading-subtitle">Heading Subtitle</h3>
+                    <h3 class="tt-heading-subtitle">Proving the concept of three-layer blockchain from a technical point of view</h3>
                 </div>
                 <!-- End tt-Heading -->
 
+                <br>
+
+                <div class="tt-heading tt-heading-stroke tt-heading-xlg anim-fadeinup">
+                    <h2 class="tt-heading-title">March 2022</h2>
+                    <!-- You can use <br> to break a text line if needed -->
+                    <h3 class="tt-heading-subtitle">Start daily creations of Zea</h3>
+                </div>
+                <!-- End tt-Heading -->
+
+                <br>
+
+                <div class="tt-heading tt-heading-stroke tt-heading-xlg anim-fadeinup">
+                    <h2 class="tt-heading-title">May 2022</h2>
+                    <!-- You can use <br> to break a text line if needed -->
+                    <h3 class="tt-heading-subtitle">Proving the concept of currency with economic support</h3>
+                </div>
+                <!-- End tt-Heading -->
+                <br>
+
+                <div class="tt-heading tt-heading-stroke tt-heading-xlg anim-fadeinup">
+                    <h2 class="tt-heading-title">DEC 2023</h2>
+                    <!-- You can use <br> to break a text line if needed -->
+                    <h3 class="tt-heading-subtitle">Completion of the specified number of 676  Ze</h3>
+                </div>
+                <!-- End tt-Heading -->
+                <br>
+
+                <div class="tt-heading tt-heading-stroke tt-heading-xlg anim-fadeinup">
+                    <h2 class="tt-heading-title">Jan 2024</h2>
+                    <!-- You can use <br> to break a text line if needed -->
+                    <h3 class="tt-heading-subtitle">Launching the website
+                        The start of the Ze collection's auction</h3>
+                </div>
+                <!-- End tt-Heading -->
+                <br>
+
+
+                <div class="tt-heading tt-heading-stroke tt-heading-xlg anim-fadeinup">
+                    <h2 class="tt-heading-title">Feb 2024</h2>
+                    <!-- You can use <br> to break a text line if needed -->
+                    <h3 class="tt-heading-subtitle">Publication of Light Paper</h3>
+                </div>
+                <!-- End tt-Heading -->
+                <br>
+
+                <div class="tt-heading tt-heading-stroke tt-heading-xlg anim-fadeinup">
+                    <h2 class="tt-heading-title">2024</h2>
+                    <!-- You can use <br> to break a text line if needed -->
+                    <h3 class="tt-heading-subtitle">
+                        <ol>
+                            <li>Completing the technical development team</li>
+                            <li>Applying for an activity permit from the UAE</li>
+                            <li>The official birth of the Limit Foundation</li>
+                            <li>Publishing of White Paper</li>
+                            <li>Development limit engine</li>
+                            <li>Code documentation</li>
+                            <li>Development "Rest"</li>
+                            <li>Launching testnet</li>
+                            <li>The end of the last phase of the Ze auction</li>
+                            <li>Starting the process of Staking Ze and releasing tokens</li>
+
+                        </ol>
+                    </h3>
+                </div>
+                <!-- End tt-Heading -->
+                <br>
+
+                <div class="tt-heading tt-heading-stroke tt-heading-xlg anim-fadeinup">
+                    <h2 class="tt-heading-title">2025</h2>
+                    <!-- You can use <br> to break a text line if needed -->
+                    <h3 class="tt-heading-subtitle">
+                        <ol>
+                            <li>Launch Main Net</li>
+                            <li>Identification of slots</li>
+                            <li>Complete the possibility of identification (Biometric KYC)</li>
+                            <li>The official release of Rest application</li>
+                            <li>Interchain connection and IBC test for stable connection </li>
+                            <li>Transferring the Limit website to the Web 3</li>
+                            <li>Limit web3 launch platform </li>
+                            <li>Development of a dedicated payment gateway for apps and websites</li>
+
+                        </ol>
+                    </h3>
+                </div>
+                <!-- End tt-Heading -->
+                <br>
+
+                <div class="tt-heading tt-heading-stroke tt-heading-xlg anim-fadeinup">
+                    <h2 class="tt-heading-title">2026</h2>
+                    <!-- You can use <br> to break a text line if needed -->
+                    <h3 class="tt-heading-subtitle">
+                        <ol>
+                            <li>Publishing a personal limit ai assistant</li>
+                            <li>Release of Web 3 social network on the rest platform</li>
+                            <li>Beginning of activity of Lim Educational Academy</li>
+                            <li>Release of Giza Metaverse</li>
+
+                        </ol>
+                    </h3>
+                </div>
+                <!-- End tt-Heading -->
+                <br>
         </div> <!-- /.tt-row -->
 
     </div> <!-- /.tt-section-inner -->
